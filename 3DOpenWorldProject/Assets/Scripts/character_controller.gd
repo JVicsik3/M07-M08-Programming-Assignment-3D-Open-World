ca01cb931c27d0592793aed11d1155e0e0b84c3b
JVicsik3 <113311084+JVicsik3@users.noreply.github.com>
2024-05-12 20:12:56 -0400

    Add files via upload

---
+++ 3DOpenWorldProject/Assets/Scripts/character_controller.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+var mouse_sensitivity = 0.25
+var camera_rotation = Vector2()
+
+@onready var camera = $CamRoot/CamYaw/CamPitch/SpringArm3D/Camera3D
+
+const SPEED = 7.5
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+		
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "front", "back")
+	var direction = (camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_cam_root_set_cam_rotation(_cam_rotation):
+	pass # Replace with function body.
